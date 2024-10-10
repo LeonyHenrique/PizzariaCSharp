b6139a5a0043e9109841955aa0af7723d69d1866
leony <leo.ny2010@hotmail.com>
2024-10-10 15:18:17 -0300

    Criando interfaces

---
+++ Controller/BebidaController.cs
@@ -0,0 +1,8 @@
+
+namespace PizzariaCSharp.Controller
+{
+    public class BebidaController
+    {
+        
+    }
+}
--- obj/Debug/net8.0/pizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/pizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8dad1023d5b2805db68313ca8137d0c2e42f3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cc81dd311db2d7ea2e1c199d172285152b26eb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("pizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
